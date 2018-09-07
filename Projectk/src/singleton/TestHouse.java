f2747512930f0aa6c59cf6d7229fc7aac6d59288
rubayethasan38 <42503485+rubayethasan38@users.noreply.github.com>
2018-09-07 18:37:17 -0400

    Update TestHouse.java

--- Projectk/src/singleton/TestHouse.java
+++ Projectk/src/singleton/TestHouse.java
@@ -3,11 +3,11 @@ package singleton.example;
 public class TestHouse {
 
     public static void main(String[] args) {
-        House house1 = House.getSingleton().get(0);
+        House house1 = House.getSingleton().get(12);
         house1.displayHouseDim();
-        House house2 = House.getSingleton().get(1);
+        House house2 = House.getSingleton().get(39);
         house2.displayHouseDim();
-        House house3 = House.getSingleton().get(1);
+        House house3 = House.getSingleton().get(13);
         house3.displayHouseDim();
         System.out.println(house1.hashCode());
         System.out.println(house2.hashCode());
