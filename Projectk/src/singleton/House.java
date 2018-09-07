cfff707a9d2e3605cc4ae582cd8bc58e8dbb9151
rubayethasan38 <42503485+rubayethasan38@users.noreply.github.com>
2018-09-07 18:36:39 -0400

    Update House.java

--- Projectk/src/singleton/House.java
+++ Projectk/src/singleton/House.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class House {
     private House(){ }
-    private static House house1 = new House();
-    private static House house2 = new House();
+    private static House house12 = new House();
+    private static House house39 = new House();
     private static List<House> houseList = new ArrayList<House>();
 
     public static List<House> getSingleton(){
-        houseList.add(house1);
-        houseList.add(house2);
+        houseList.add(house12);
+        houseList.add(house39);
         return houseList;
     }
     public void displayHouseDim(){
